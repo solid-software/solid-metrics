374e77c18f9ecea8a4e6699b0f907c3a931a21c0
tomboichik <tomboffos@gmail.com>
2023-07-19 14:40:56 +0600

    added solid lints and custom lint builder

--- lib/complexity_metric.dart
+++ lib/complexity_metric.dart
@@ -1,7 +1,7 @@
 library complexity_metric;
 
-/// A Calculator.
-class Calculator {
+/// A Complexity metric.
+class ComplexityMetric {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
--- test/complexity_metric_test.dart
+++ test/complexity_metric_test.dart
@@ -1,10 +1,9 @@
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:complexity_metric/complexity_metric.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
+    final calculator = ComplexityMetric();
     expect(calculator.addOne(2), 3);
     expect(calculator.addOne(-7), -6);
     expect(calculator.addOne(0), 1);
